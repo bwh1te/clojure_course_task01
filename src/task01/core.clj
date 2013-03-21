63feb77a431ac36c2c78974390ebe1ec79ba120f
bwh1te <aka.bwhite@gmail.com>
2013-03-21 11:34:20 +0700

    Solution

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -2,12 +2,14 @@
   (:require [pl.danieljanus.tagsoup :refer :all])
   (:gen-class))
 
-
 (defn get-links []
-  (let [data (parse "clojure_google.html")]
-    nil))
+  (let [data (parse "clojure_google.html")
+        result-blocks #(= (:class (attributes %)) "r")
+        get-url #(:href ((% 2) 1))]
+    (map get-url
+      (filter
+        result-blocks
+        (tree-seq #(keyword? (first %)) rest data)))))
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
-
-
